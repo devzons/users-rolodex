81841b375469d3cdeb7d011c32fe67562192ec81
Don Yu <devzons@gmail.com>
2019-09-24 04:03:05 -0700

    LIVE CYCLE: fetch external data <componentDidMount>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({users: users}))
+  }
+
+  render() {    
+    return (
+      <div className="App">
+        {
+          this.state.users.map(user => <h1 key={user.id}>{ user.name }</h1>)
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
