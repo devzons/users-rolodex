927d96dce493799127de3c4f4a6651c60d4761ee
Don Yu <devzons@gmail.com>
2019-09-24 04:56:31 -0700

    Create card-list component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CardList } from './components/card-list/card-list.component'
 import './App.css'
 
 class App extends React.Component {
@@ -19,9 +20,10 @@ class App extends React.Component {
   render() {    
     return (
       <div className="App">
-        {
-          this.state.users.map(user => <h1 key={user.id}>{ user.name }</h1>)
-        }
+        <CardList>
+        {this.state.users.map(user => (<h1 key={user.id}>{ user.name }</h1>))}
+        </CardList>
+        
       </div>
     )
   }
