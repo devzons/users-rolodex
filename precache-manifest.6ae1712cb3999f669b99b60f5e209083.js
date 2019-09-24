7664a7c25ac9414630267bd32ecf9dadccf404e9
Don Yu <devzons@gmail.com>
2019-09-24 14:28:43 -0700

    Updates

---
+++ precache-manifest.6ae1712cb3999f669b99b60f5e209083.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d49b4c781b3a2d1a264a96485b84a4",
+    "url": "/users-rolodex/index.html"
+  },
+  {
+    "revision": "18531c8eff8a6d3b3750",
+    "url": "/users-rolodex/static/css/main.7e397847.chunk.css"
+  },
+  {
+    "revision": "7271eec0dec5ac34b342",
+    "url": "/users-rolodex/static/js/2.446e5f0f.chunk.js"
+  },
+  {
+    "revision": "18531c8eff8a6d3b3750",
+    "url": "/users-rolodex/static/js/main.1d490886.chunk.js"
+  },
+  {
+    "revision": "ed5abb4e857ba1b4feb3",
+    "url": "/users-rolodex/static/js/runtime-main.baaf3ee9.js"
+  }
+]);
